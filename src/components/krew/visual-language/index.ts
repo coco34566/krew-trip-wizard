bed29c360b024a78b7dfcafe1952878e5f157afb
Grok 4 <nestpointimmo@gmail.com>
2026-08-19 07:50:30 +0200

    Export KREW concept icons

--- src/components/krew/visual-language/index.ts
+++ src/components/krew/visual-language/index.ts
@@ -1,3 +1,5 @@
 export { KrewMark } from "./KrewMark";
 export type { KrewMarkSize, KrewMarkTone, KrewMarkType } from "./KrewMark";
 export { KrewAnnotation, KrewConnector, KrewHighlight, KrewPhotoOverlay } from "./KrewPrimitives";
+export { KrewIcon } from "./KrewIcon";
+export type { KrewIconName, KrewIconSize, KrewIconTone } from "./KrewIcon";
