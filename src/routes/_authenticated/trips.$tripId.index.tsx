c13890eb13d7cffcbbd9d7462008e69cc077364f
Grok 4 <nestpointimmo@gmail.com>
2026-09-03 10:47:44 +0200

    Forcer la largeur réelle de la vague Membres du groupe (#321)
    * Use dedicated class for group title underline
    
    * Force visible width for group heading wave

--- src/routes/_authenticated/trips.$tripId.index.tsx
+++ src/routes/_authenticated/trips.$tripId.index.tsx
@@ -1260,7 +1260,7 @@ function TripDetail() {
               </div>
 
               {/* Underline wave KrewMark sous le titre */}
-              <KrewMark type="underline-wave" tone="sage" size="sm" className="mt-1 h-[10px] w-[210px] opacity-90 pointer-events-none sm:w-[260px]" />
+              <KrewMark type="underline-wave" tone="sage" size="sm" className="krew-group-title-underline mt-1 opacity-90 pointer-events-none" />
             </div>
 
             <ul className="divide-y divide-border/40 pt-1">
