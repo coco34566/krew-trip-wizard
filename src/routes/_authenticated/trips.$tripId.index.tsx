f8a8ead202e9ce785863c3a2c0346234e85a217c
Grok 4 <nestpointimmo@gmail.com>
2026-09-03 08:24:06 +0200

    Widen group section Krew underline

--- src/routes/_authenticated/trips.$tripId.index.tsx
+++ src/routes/_authenticated/trips.$tripId.index.tsx
@@ -1260,7 +1260,7 @@ function TripDetail() {
               </div>
 
               {/* Underline wave KrewMark sous le titre */}
-              <KrewMark type="underline-wave" tone="sage" size="sm" className="w-[100px] h-[8px] mt-1 opacity-85 pointer-events-none" />
+              <KrewMark type="underline-wave" tone="sage" size="sm" className="mt-1 h-[8px] w-[150px] opacity-85 pointer-events-none sm:w-[190px]" />
             </div>
 
             <ul className="divide-y divide-border/40 pt-1">
